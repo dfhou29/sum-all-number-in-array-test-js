5a969b45be3590b894a1939409a68dddbd0b69a7
Difan <dfhou29@gmail.com>
2023-07-28 21:11:23 -0700

    sum all items in a array recursively.

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 function sumItems(array) {
+  let sum = 0;
+  for (const item of array) {
+    if (Array.isArray(item)) {
+      sum+= sumItems((item));
+    } else {
+      sum += item;
+    }
+  }
+  return sum;
   // Sum all the numbers in the array
 }
 
